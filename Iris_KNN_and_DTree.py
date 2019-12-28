d8665cd6ec400996a30e84406f5f40d7359ebdce
adityamon11 <adityakulkarni270@gmail.com>
2019-12-28 15:08:59 +0530

    Add files via upload

---
+++ Iris_KNN_and_DTree.py
@@ -0,0 +1,49 @@
+from sklearn import tree;
+from sklearn.datasets import load_iris;
+from sklearn.metrics import accuracy_score;
+from sklearn.model_selection import train_test_split;
+from sklearn.neighbors import KNeighborsClassifier;
+
+
+
+def CalculateDecisionTreeAccuracy():
+	iris = load_iris();
+
+	data = iris.data;
+	target = iris.target;
+
+	data_train,data_test,target_train,target_test = train_test_split(data,target,test_size=0.75);
+
+	classifier = tree.DecisionTreeClassifier();
+	classifier.fit(data_train,target_train);
+	predictions = classifier.predict(data_test);
+
+	accuracy = accuracy_score(target_test,predictions);
+	return  accuracy;
+
+def CalculateKNNAccuracy():
+	iris = load_iris();
+
+	data = iris.data;
+	target = iris.target;
+
+	data_train,data_test,target_train,target_test = train_test_split(data,target,test_size=0.75);
+
+	classifier = KNeighborsClassifier();
+	classifier.fit(data_train,target_train);
+	predictions = classifier.predict(data_test);
+
+	accuracy = accuracy_score(target_test,predictions);
+	return  accuracy;
+
+
+def main():
+	accuracy = CalculateDecisionTreeAccuracy();
+	print("Accuracy using Decision Tree algorithm is::",accuracy *100,"%");
+	accuracy = CalculateKNNAccuracy();
+	print("Accuracy using K-Nearest Neighbour algorithm is::",accuracy *100,"%");
+
+
+
+if __name__ == '__main__':
+	main()
